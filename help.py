c09cd792273b17edd6b59b1a38694e2400463999
soroush saljooghi <soroushs1382@gmail.com>
2023-09-18 18:29:54 +0330

    photo project (#33)

--- help.py
+++ help.py
@@ -1,6 +1,7 @@
 import cv2
-import cv2
-image=cv2.imread("E:\\u\\python\\hello\\photo_2023-01-07_18-26-51.jpg")
-a=cv2.imshow("photo_2023-01-07_18-26-51",image)
+
+
+image=cv2.imread("zoro.jpg")
+a=cv2.imshow("zoro",image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
