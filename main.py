64ae733328a304566a2516cb9cce72606f51fc73
soroushhaghi <soroushhaghi1378@gmail.com>
2023-09-18 19:56:39 +0330

    applying 3 steps #35

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ cv2.waitKey(0)
 canny=cv2.Canny(p,150,100)
 cv2.imshow('canny',canny)
 cv2.waitKey(0)
-cont=cv2.multiply(p,5)
+cont=p
+cont[:,:,2]=cont[:,:,2]+50
+cont[:,:,1]=cont[:,:,1]+50
+cont[:,:,0]=cont[:,:,0]+50
 cv2.imshow('cont',cont)
 cv2.waitKey(0)
