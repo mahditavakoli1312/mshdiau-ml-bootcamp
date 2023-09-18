fcfebabaae73fbf572daadec8231ea43de9bc663
soroush saljooghi <soroushs1382@gmail.com>
2023-09-18 19:53:07 +0330

    photo prosesing (#35)

--- help.py
+++ help.py
@@ -1,7 +1,27 @@
+import numpy as np
 import cv2
 
 
-image=cv2.imread("zoro.jpg")
-a=cv2.imshow("zoro",image)
+image = cv2.imread('E:\\pictures\\anime\\18840848725573.jpg')
+
+
+dst = cv2.GaussianBlur(image, (5 , 5) , 0)
+
+cv2.imshow('Fast Means Denoising', dst)
+cv2.imshow('original image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# #تشخیص لبه
+dst = cv2.Canny(image, 50, 150)
+
+cv2.imshow('Fast Means Denoising', dst)
+cv2.imshow('original image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#کنتراست
+dst = cv2.multiply(image, 2.0)
+
+cv2.imshow('Fast Means Denoising', dst)
+cv2.imshow('original image', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
