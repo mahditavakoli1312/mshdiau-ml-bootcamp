26c8bfd13d22cd8160bb730d4e38365e1a947080
faraz <faraz.tajdar2@gmail.com>
2023-09-18 16:49:04 -0700

    createcalculator (#16)

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-#"Hi_world"
+def calculator():
+    print("Welcome to the calculator!")
+    print("Enter 'q' to quit.")
+
+    while True:
+        operation = input("please enter the operation (+, -, *, /)")
+        if operation == 'q':
+            print ( "GoodBye!" )
+            break
+
+
+            num1 = float(input("please enter the first number: "))
+            num2 = float(input("please enter the second number: "))
+
+            result = 0
+
+            if operation == '+':
+                result = num1 + num2
+            elif operation == '-' :
+                result = num1 - num2
+            elif operation == '*' :
+                result = num1 * num2
+            elif operation == '/':
+                if num2 != 0:
+                    result = num1 / num2
+                else:
+                    print("Error: division by zero!")
+                    continue
+                    print("result: ", result)
+                    print("_____________________")
+calculator()
