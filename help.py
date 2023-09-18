280cb6fa228725786e0b8021943897ba7ae9dfd5
soroush saljooghi <soroushs1382@gmail.com>
2023-09-18 18:22:56 +0330

    photo project (#33)

---
+++ help.py
@@ -0,0 +1,6 @@
+import cv2
+import cv2
+image=cv2.imread("E:\\u\\python\\hello\\photo_2023-01-07_18-26-51.jpg")
+a=cv2.imshow("photo_2023-01-07_18-26-51",image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
