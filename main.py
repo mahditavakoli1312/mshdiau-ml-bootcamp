ea099c30e5cde46f5677b99bb0e2f15ef1b57123
Morteza <smseyyednnejad@gmail.com>
2023-09-18 19:51:23 +0330

    ImageProcessing (#37)

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+img = cv2.imread('download.jpg')
+cv2.imshow('img',img)
+cv2.waitKey(0)
+edge = cv2.Canny(img,50,100)
+cv2.imshow('edges',edge)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+blurred = cv2.GaussianBlur(img , (5,5),0)
+cv2.imshow('Blurred',blurred)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+contrastform = cv2.multiply(img,5)
+cv2.imshow('Contrast',contrastform)
+cv2.waitKey(0)
+
+#cv2.imshow('img',img)
+#cv2.waitKey(0)
