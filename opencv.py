5dd09775077b0117bdf0d294bacdf51d3e5b546f
MoienSD <moiensohani80@gmail.com>
2023-09-18 18:30:02 +0330

    OpenCv (#31)

--- opencv.py
+++ opencv.py
@@ -3,9 +3,5 @@ import cv2
 
 image = cv2.imread('Ghossy.jpg')
 
-while True:
-    cv2.imshow("Ghossy", image)
-
-    key = cv2.waitKey(0)
-    if key == 27:
-        break
+cv2.imshow("Ghossy", image)
+key = cv2.waitKey(0)
