36b9c2f99d32d941ca80bf593cd2cde00ea79dfe
Morteza <smseyyednnejad@gmail.com>
2023-09-18 18:18:30 +0330

    opencv test (#26)

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+GOAT = cv2.imread(r'C:\Users\Dell\Desktop\download.jpg')
+cv2.imshow('GOAT', GOAT)
+cv2.waitKey(0)
+
+#   while (True):
+ #       ocv.imshow('GOAT', GOAT)
+  #      k = ocv.waitKey(1)
+   #     if k == 27:
+    #        break
+
+# C:\Users\Dell\Desktop\download.jpg
+# C:\Users\Dell\Desktop\Hausziege_04.jpg
