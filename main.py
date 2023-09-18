9be239efdf36084945684690f1e36ce0a5720881
mahboobeh ebrahimi <matinebrahimi0707@gmail.com>
2023-09-18 19:42:25 +0330

    delete noise (#41)

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#test
+import cv2
+import numpy as np
+
+# خواندن تصویر
+image = cv2.imread('download.jpeg')
+
+# اعمال فیلتر میانگین با اندازه مشخص
+kernel_size = 5  # اندازه فیلتر میانگین
+denoised_image = cv2.blur(image, (kernel_size, kernel_size))
+
+# نمایش تصویر حاصل
+cv2.imshow('Denoised Image', denoised_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
