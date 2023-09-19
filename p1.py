8ba19a4914bbcd643a48decc5ce13b2310e264d6
Morteza <smseyyednnejad@gmail.com>
2023-09-19 17:21:05 +0330

    Histogram Plot Revised (#48)

--- p1.py
+++ p1.py
@@ -1,21 +1,22 @@
 import cv2
 import matplotlib.pyplot as plt
-img1 = cv2.imread('white.png')
+
+img1 = cv2.imread('white.jpg')
 NewImg1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 hist1 = cv2.calcHist(NewImg1,[0],None,[256],[0,256])
 
-img2 = cv2.imread('black.png')
+img2 = cv2.imread('black.jpg')
 NewImg2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
 hist2 = cv2.calcHist(NewImg2,[0],None,[256],[0,256])
 
 plt.plot(hist1)
-plt.title('histogram')
+plt.title('histogram(white)')
 plt.xlabel("intensity")
 plt.ylabel('pixels')
 plt.show()
 
 plt.plot(hist2)
-plt.title('histogram')
+plt.title('histogram(black)')
 plt.xlabel("intensity")
 plt.ylabel('pixels')
 plt.show()
