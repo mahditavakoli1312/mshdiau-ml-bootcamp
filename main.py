c806d680c7ae19cffb4e9cead9ddb687d0c236ab
HP <zahraabedi303@gmail.com>
2023-09-19 09:03:46 -0700

    detecting faces2 (#61)

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import cv2
 from cv2 import CascadeClassifier
 
-imagePath = 'faces.jpg'
+imagePath = 'faces2.jpg'
 
 img = cv2.imread(imagePath)
 
 cascades = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-faces = cascades.detectMultiScale(img,scaleFactor=1.1, minNeighbors=5, maxSize=(130,130))
+faces = cascades.detectMultiScale(img,scaleFactor=1.1, minNeighbors=1, maxSize=(100,100))
 
 for(x,y,w,h)in faces :
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
 
 cv2.imshow("result",img)
 cv2.waitKey(0)
