139ab45988bb1446c1a2ac5296aa732776998e76
Morteza <smseyyednnejad@gmail.com>
2023-09-19 19:22:35 +0330

    Face Recognition Optimum (#57)

--- ex5.py
+++ ex5.py
@@ -2,9 +2,10 @@ import cv2
 
 cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread('faces.jpg')
-face = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=1, maxSize=(1, 1))
+face = cascade.detectMultiScale(img, scaleFactor=1.01, minNeighbors=10, minSize=(5,5 ))
+#face = cascade.detectMultiScale(img, 1.3, 5)
 for x, y, w, h in face:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 4)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 4)
 
 cv2.imshow('result', img)
 cv2.waitKey(0)
