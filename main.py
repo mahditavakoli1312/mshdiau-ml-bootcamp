1da2ce51167f80711f8df7518d2a4e8ca090c539
HP <zahraabedi303@gmail.com>
2023-09-19 06:52:50 -0700

    histogram (#46)

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-# خواندن تصویر
-image = cv2.imread('sib.jpg')
+# عکس مشکی
+image1 = cv2.imread('b.jpg')
 
-# اعمال فیلتر میانگین با اندازه مشخص
-kernel_size = 5  # اندازه فیلتر میانگین
-denoised_image = cv2.blur(image, (kernel_size, kernel_size))
-
-# نمایش تصویر حاصل
-cv2.imshow('Denoised Image', denoised_image)
+gray_image = cv2.cvtColor(image1, cv2.COLOR_BGRA2GRAY)
+cv2.imshow("Grayscale", gray_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+hist = cv2.calcHist([gray_image], [0], None, [256], [0, 255])
+cv2.imshow("image", gray_image)
+
+plt.plot(hist)
+plt.title("Histogram")
+plt.xlabel("Intensity")
+plt.ylabel("Number of pixels")
+plt.show()
+cv2.waitKey(0)
+
+#عکس سفید
+
+image2 = cv2.imread('w.jpg')
+
+gray_image = cv2.cvtColor(image2, cv2.COLOR_BGRA2GRAY)
+cv2.imshow("Grayscale", gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+hist = cv2.calcHist([gray_image], [0], None, [256], [0, 255])
+cv2.imshow("image", gray_image)
+
+plt.plot(hist)
+plt.title("Histogram")
+plt.xlabel("Intensity")
+plt.ylabel("Number of pixels")
+plt.show()
+cv2.waitKey(0)
