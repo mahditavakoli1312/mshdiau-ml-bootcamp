15fb91fb855f3fec7ba5950b25c6bd176a183c85
mohammadRobati <mohammadrbt1@gmail.com>
2023-09-19 07:51:24 -0400

    photo project (#42)

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import numpy as np
 import cv2
-image=cv2.imread("wallpaperflare.com_wallpaper.jpg")
-cv2.imshow("wallpaperflare.com_wallpaper",image)
+
+image = cv2.imread('Beautiful Mixed.jpg')
+
+dst = cv2.GaussianBlur(image, (5,5),0)
+
+cv2.imshow('Fast Means Denoising', dst)
+cv2.imshow('original image', image)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+
+
+dst = cv2.Canny(image,50,150)
+
+cv2.imshow('Fast Means Denoising', dst)
+cv2.imshow('original image', image)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+
+dst = cv2.multiply(image, 2.0)
+
+cv2.imshow('Fast Means Denoising', dst)
+cv2.imshow('original image', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
+
 
