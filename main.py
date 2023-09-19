e1c41c9a9eeb508f90685b76c12ca44af50d2ae9
mohammadRobati <mohammadrbt1@gmail.com>
2023-09-19 06:22:13 -0400

    photo project (#32)

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-#test
-number_1 = int(input('Enter your first number: '))
-number_2 = int(input('Enter your second number: '))
-
-# Addition
-print('{} + {} = '.format(number_1, number_2))
-print(number_1 + number_2)
-
-# Subtraction
-print('{} - {} = '.format(number_1, number_2))
-print(number_1 - number_2)
-
-# Multiplication
-print('{} * {} = '.format(number_1, number_2))
-print(number_1 * number_2)
-
-# Division
-print('{} / {} = '.format(number_1, number_2))
-print(number_1 / number_2)
+import cv2
+image=cv2.imread("wallpaperflare.com_wallpaper.jpg")
+cv2.imshow("wallpaperflare.com_wallpaper",image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
